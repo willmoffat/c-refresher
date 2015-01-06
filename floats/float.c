fbdf50ee15d375d4be7a6d1ffe6ccc4048b7e002
Will Moffat <will@hamstersoup.com>
2015-01-06 12:01:41 +0100

    Clang-format

--- floats/float.c
+++ floats/float.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
-//assumes little endian
-void p(float *ptr)
-{
-  unsigned char *b = (unsigned char*) ptr;
+// assumes little endian
+void p(float *ptr) {
+  unsigned char *b = (unsigned char *)ptr;
   unsigned char byte;
   int i, j;
   size_t size = 4;
 
-  printf("\nfloat=%f\n",*ptr);
-  for (i=size-1;i>=0;i--)
-    {
-      byte = b[i];
-      printf("   %x", byte >> 4);
-      printf("    %x ", byte & 0xf);
-    }
+  printf("\nfloat=%f\n", *ptr);
+  for (i = size - 1; i >= 0; i--) {
+    byte = b[i];
+    printf("   %x", byte >> 4);
+    printf("    %x ", byte & 0xf);
+  }
   puts("");
-  for (i=size-1;i>=0;i--)
-    {
-      for (j=7;j>=0;j--)
-        {
-	  byte = b[i] & (1<<j);
-	  byte >>= j;
-	  printf("%d", byte);
-	  if (j==4) {printf(" "); }
-        }
-      printf(" ");
+  for (i = size - 1; i >= 0; i--) {
+    for (j = 7; j >= 0; j--) {
+      byte = b[i] & (1 << j);
+      byte >>= j;
+      printf("%d", byte);
+      if (j == 4) {
+        printf(" ");
+      }
     }
+    printf(" ");
+  }
   puts("");
 }
 
 int main() {
-  printf("sizeof(float) %d\n", (int)sizeof(float));
+  printf("sizeof(float)=%d\n", (int)sizeof(float));
 
-  float f;
+  float a,b,c;
+  a = 1.0f;
+  b = 0.5f;
+  c = 2.0f;
 
-  f = 1; p(&f);
-  f = 2; p(&f);
-  f = 0.5; p(&f);
-  //  f = 2; p(&f);
-  // f = 4; p(&f);
+  p(&a);
+  p(&b);
+  p(&c);
 
   return 0;
 }
